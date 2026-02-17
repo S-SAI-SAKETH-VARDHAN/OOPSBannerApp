e8d78f15000a5954b99c634a1b3116e62209d143
S-SAI-SAKETH-VARDHAN <as6770@srmist.edu.in>
2026-02-17 09:08:48 +0530

    Add UC5 implementation

---
+++ OOPSPrint.java
@@ -0,0 +1,21 @@
+public class OOPSPrint {
+
+    public static void main(String[] args) {
+
+        String[] bannerLines = {
+            String.join(" ", "   ***", "       ***", "     ******", "       *****"),
+            String.join(" ", " **   **", "   **   **", "   **    **", "    **"),
+            String.join(" ", "**     **", " **     **", "  **     **", "  **"),
+            String.join(" ", "**     **", " **     **", "  **    **", "    **"),
+            String.join(" ", "**     **", " **     **", "  ******", "       ***"),
+            String.join(" ", "**     **", " **     **", "  **", "             **"),
+            String.join(" ", "**     **", " **     **", "  **", "              **"),
+            String.join(" ", " **   **", "   **   **", "  **", "             **"),
+            String.join(" ", "   ***", "       ***", "     **", "        *****")
+        };
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
